cbdcc9adc76cbad85d4b7fdcfe36d4f94eeed8e0
Тимофей Тимошевский <timkatimtimhok@mail.ru>
2022-08-05 15:02:53 +0300

    update src

--- front-brawl-picker/src/components/BrawlTierList.js
+++ front-brawl-picker/src/components/BrawlTierList.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-<<<<<<< HEAD
 function BrawlTierList () {
     return(
         <h1 className="text-xl font-medium mb-1 text-center">BrawlTierList</h1>
@@ -8,12 +7,3 @@ function BrawlTierList () {
 }
 
 export default BrawlTierList
-=======
-function Profile (props) {
-    return(
-        <h1 className="text-xl font-medium mb-1 text-center">Profile {props.data.name}</h1>
-    )
-}
-
-export default Profile
->>>>>>> fac9f429b088a911c15a7f7bed0003dea00c1a2c
