98cb8bee7b1fb3c7e1217d37fc6bdfa66e01a382
Mike <mkuimovk@mail.ru>
2022-08-05 14:05:10 +0300

    push front

--- front-brawl-picker/src/App.js
+++ front-brawl-picker/src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 import axios from "axios";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
 import Header from "./components/Header";
-
+import Profile from "./components/Profile";
+import MapTierList from "./components/MapTierList";
+import BrawlTierList from "./components/BrawlTierList";
 
 function App () {
 
@@ -11,10 +15,19 @@ function App () {
 
     const [show_tag, setShow_tag] = React.useState(false)
 
+    const [data, setData] = React.useState(JSON.parse(localStorage.getItem("data_tag")) || {
+        data: ''
+    })
 
+    //const [best_try, setBest_try] = React.useState(JSON.parse(localStorage.getItem("data_tag")) || {data: ''})
+
+
+    // if (roll_count<best_try) {
+    //     localStorage.setItem("data_tag", JSON.stringify(roll_count))
+    // }
 
     function Keep_tag (event) {
-        const {name, value, type} = event.target
+        const {name, value} = event.target
         setTag(prev => {
             return{
                 ...prev,
@@ -23,62 +36,75 @@ function App () {
         })
     }
 
-    const res = e =>{
-        e.preventDefault()
-
-    }
-
     const API_URL = 'http://127.0.0.1:8000/api/'
 
+    //Отпрака тега на backend
     function sendToBack() {
         axios({
             method: 'post',
             url: API_URL,
             data: tag,
         })
-            .then(res => console.log(res))
+            //.then(res => console.log(res))
+            .then(res => setData(res))
             .catch(err => console.error(err))
+        setShow_tag(prev => !prev)
+        //console.log(show_tag)
+        //localStorage.setItem("data_tag", JSON.stringify(data))
+        //console.log(data.data[0])
     }
-    const [data,setData] = React.useState({
-        data:''
-    })
 
-    React.useEffect( () => {
-        async function getFromBAck() {
-            console.log('getFromBAck')
-            const response = await axios({
-                method: 'get',
-                url: API_URL
-            })
-                .then(res => console.log(res))
-                .catch(err => console.error(err))
-        }
-        getFromBAck()
+    React.useEffect ( () => {
+        localStorage.setItem("data_tag", JSON.stringify(data))
+        //localStorage.clear()
     }, [data])
 
-        // axios.post(API_URL, tag)
-        //     .then(function (response) {
-        //         console.log(response);
-        //     })
-        //     .catch(function (error) {
-        //         console.log(error);
-        //     });
 
-    function Take_tag (){
-        setShow_tag(prev => !prev)
-    }
+
+    //Сбор данных с сервера backend
+    // React.useEffect( () => {
+    //     async function getFromBAck() {
+    //         await axios({
+    //             method: 'get',
+    //             url: API_URL
+    //         })
+    //             .then(res => setData(res))
+    //             .catch(err => console.error(err))
+    //     }
+    //     getFromBAck()
+    //     console.log(data.data[0])
+    //     localStorage.setItem("data_tag", JSON.stringify(data))
+    //     console.log("state_from_back:", data)
+    // }, [])
 
     return(
         <div>
             <Header />
-            <main>
-                <div className="input_there">
-                    <h1>Input will be there!</h1>
-                    <input className="input_tag" type="text" placeholder="Enter your Tag" name="ent_tag" value={tag.ent_tag} onChange={Keep_tag} />
-                    <button className="btn_tag" onClick={sendToBack} > Search </button>
-                    {show_tag ? <h1>{tag.ent_tag}</h1> : <></>}
-                </div>
-            </main>
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <Home
+                            name={"ent_tag"}
+                            value={tag.ent_tag}
+                            handleChange={Keep_tag}
+                            handleClick={sendToBack}
+                            //data={data.data[0]}
+                        />}
+                />
+                <Route
+                    path="/Profile"
+                    element={<Profile data={data.data[0]} />}
+                />
+                <Route
+                    path="/BrawlTierList"
+                    element={<BrawlTierList/>}
+                />
+                <Route
+                    path="/MapTierList"
+                    element={<MapTierList/>}
+                />
+            </Routes>
         </div>
     )
 }
