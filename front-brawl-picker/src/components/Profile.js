7cde1fb3ca4318e2fb2277bdcb5bcfd21f11ca57
Тимофей Тимошевский <timkatimtimhok@mail.ru>
2022-09-06 23:09:59 +0300

    update brawlers

--- front-brawl-picker/src/components/Profile.js
+++ front-brawl-picker/src/components/Profile.js
@@ -85,22 +85,26 @@ function Profile () {
                         <h1 className="text-xl font-medium my-2 mb-4">Brawlers</h1>
                         <h1 className="text-lg font-normal my-2 mb-4">Important info about your all Brawlers!</h1> 
                     </div>
-                    <div className="Brawlers_block">                     
-                    {Boolean(brawlers.length) &&
-                        brawlers[0].map((item) =>
-                            <div className="Brawler_card">
-                                <h2 className="Brawler_card_name">{item.name}</h2>
-                                <img src="https://cdn.brawlify.com/brawler/Bibi.png" className="Brawler_card_img" alt="icon"/>
-                                <div className="Brawler_card_stats">
-                                    <ul className="ul_Brawler_card_stats">
-                                        <li><span>Power Level</span> <span className="span_Profile_card_stats"> <img src="https://cdn.brawlify.com/icon/Ranking.png" className="w-4 h-4" alt="new"/> {item.power}</span></li>
-                                        <li><span>Rank</span> <span>{item.rank}</span></li>
-                                        <li><span>Highest Trophies</span> <span className="span_Profile_card_stats"> <img src="https://cdn.brawlify.com/icon/trophy.png" className="w-4 h-4" alt="new"/> {item.highest_trophies}</span></li>
-                                        <li><span>Trophies</span> <span className="span_Profile_card_stats"> <img src="https://cdn.brawlify.com/icon/trophy.png" className="w-4 h-4" alt="new"/> {item.trophies}</span></li>
-                                    </ul>
-                                </div>
-                            </div>)
-                    }
+                    <div className="Brawlers_block"> 
+                        <button className="handle handle_left"></button>
+                        <div className="Brawlers_slider">
+                            {Boolean(brawlers.length) &&
+                                brawlers[0].map((item) =>
+                                    <div className="Brawler_card">
+                                        <h2 className="Brawler_card_name">{item.name}</h2>
+                                        <img src="https://cdn.brawlify.com/brawler/Bibi.png" className="Brawler_card_img" alt="icon"/>
+                                        <div className="Brawler_card_stats">
+                                            <ul className="ul_Brawler_card_stats">
+                                                <li><span>Power Level</span> <span className="span_Profile_card_stats"> <img src="https://cdn.brawlify.com/icon/Ranking.png" className="w-4 h-4" alt="new"/> {item.power}</span></li>
+                                                <li><span>Rank</span> <span>{item.rank}</span></li>
+                                                <li><span>Highest Trophies</span> <span className="span_Profile_card_stats"> <img src="https://cdn.brawlify.com/icon/trophy.png" className="w-4 h-4" alt="new"/> {item.highest_trophies}</span></li>
+                                                <li><span>Trophies</span> <span className="span_Profile_card_stats"> <img src="https://cdn.brawlify.com/icon/trophy.png" className="w-4 h-4" alt="new"/> {item.trophies}</span></li>
+                                            </ul>
+                                        </div>
+                                    </div>)
+                            }   
+                        </div>                    
+                        <button className="handle handle_right"></button>
                     </div> 
                 </div>
             </div>
